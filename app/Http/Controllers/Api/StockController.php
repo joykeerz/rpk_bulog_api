534cede25ac23d15dfe478c70658471569f5dcb7
joykeerz <wisnu.mukti10@gmail.com>
2024-04-26 08:56:46 +0700

    removed clutters

--- app/Http/Controllers/Api/StockController.php
+++ app/Http/Controllers/Api/StockController.php
@@ -12,21 +12,6 @@ class StockController extends Controller
 {
     public function getAllStocks()
     {
-        // return $satuan_unit = DB::table('satuan_unit')
-        //     ->select('satuan_unit.*')
-        //     ->get();
-
-        // $stocks = DB::table('stok')
-        //     ->join('produk', 'stok.produk_id', '=', 'produk.id')
-        //     ->join('gudang', 'stok.gudang_id', '=', 'gudang.id')
-        //     ->join('kategori', 'produk.kategori_id', '=', 'kategori.id')
-        //     ->join('satuan_unit', 'produk.satuan_unit_id', '=', 'satuan_unit.id')
-        //     ->join('pajak', 'produk.pajak_id', '=', 'pajak.id')
-        //     ->select('stok.*', 'produk.*', 'gudang.*', 'kategori.*', 'pajak.jenis_pajak', 'pajak.nama_pajak', 'pajak.persentase_pajak', 'stok.id as sid', 'produk.id as pid', 'gudang.id as gid', 'kategori.id as kid', 'stok.created_at as cat')
-        //     ->orderBy('cat', 'desc')
-        //     // ->simplePaginate(1);
-        //     ->get();
-
         $stokEtalase = DB::table('stok_etalase')
             ->join('stok', 'stok.id', 'stok_etalase.stok_id')
             ->join('prices', 'prices.id', 'stok.id')
@@ -70,18 +55,6 @@ class StockController extends Controller
 
     public function getStocksByCategory($id)
     {
-        // $stocks = DB::table('stok')
-        //     ->join('produk', 'stok.produk_id', '=', 'produk.id')
-        //     ->join('gudang', 'stok.gudang_id', '=', 'gudang.id')
-        //     ->join('kategori', 'produk.kategori_id', '=', 'kategori.id')
-        //     ->join('satuan_unit', 'produk.satuan_unit_id', '=', 'satuan_unit.id')
-        //     ->join('pajak', 'produk.pajak_id', '=', 'pajak.id')
-        //     ->select('satuan_unit.satuan_unit_produk', 'stok.*', 'produk.*', 'gudang.*', 'kategori.*', 'pajak.jenis_pajak', 'pajak.nama_pajak', 'pajak.persentase_pajak', 'stok.id as sid', 'produk.id as pid', 'gudang.id as gid', 'kategori.id as kid', 'stok.created_at as cat')
-        //     ->where('produk.kategori_id', '=', $id)
-        //     ->orderBy('cat', 'desc')
-        //     // ->paginate(10);
-        //     ->get();
-
         $stokEtalase = DB::table('stok_etalase')
             ->join('stok', 'stok.id', 'stok_etalase.stok_id')
             ->join('prices', 'prices.id', 'stok.id')
@@ -124,20 +97,6 @@ class StockController extends Controller
 
     public function getStocksByCategoryAndGudang($id, $gid)
     {
-        // return $id .' '. $gid;
-        // $stocks = DB::table('stok')
-        //     ->join('produk', 'stok.produk_id', '=', 'produk.id')
-        //     ->join('gudang', 'stok.gudang_id', '=', 'gudang.id')
-        //     ->join('kategori', 'produk.kategori_id', '=', 'kategori.id')
-        //     ->join('satuan_unit', 'produk.satuan_unit_id', '=', 'satuan_unit.id')
-        //     ->join('pajak', 'produk.pajak_id', '=', 'pajak.id')
-        //     ->select('satuan_unit.satuan_unit_produk', 'stok.*', 'produk.*', 'gudang.*', 'kategori.*', 'pajak.jenis_pajak', 'pajak.nama_pajak', 'pajak.persentase_pajak', 'stok.id as sid', 'produk.id as pid', 'gudang.id as gid', 'kategori.id as kid', 'stok.created_at as cat')
-        //     ->where('produk.kategori_id', '=', $id)
-        //     ->where('gudang.id', '=', $gid)
-        //     ->orderBy('cat', 'desc')
-        //     // ->paginate(10);
-        //     ->get();
-
         $stokEtalase = DB::table('stok_etalase')
             ->join('stok', 'stok.id', 'stok_etalase.stok_id')
             ->join('prices', 'prices.id', 'stok.id')
@@ -313,16 +272,6 @@ class StockController extends Controller
                 'error' => $validator->errors(),
             ], 400);
         }
-
-        // $stocks = DB::table('stok')
-        //     ->join('produk', 'stok.produk_id', '=', 'produk.id')
-        //     ->join('gudang', 'stok.gudang_id', '=', 'gudang.id')
-        //     ->join('kategori', 'produk.kategori_id', '=', 'kategori.id')
-        //     ->select('stok.*', 'produk.*', 'gudang.*', 'kategori.*', 'stok.id as sid', 'produk.id as pid', 'gudang.id as gid', 'kategori.id as kid', 'stok.created_at as cat')
-        //     ->where('produk.nama_produk', 'like', '%' . $request->nama_produk . '%')
-        //     ->orderBy('cat', 'desc')
-        //     // ->paginate(10);
-        //     ->get();
 
         $stokEtalase = DB::table('stok_etalase')
             ->join('stok', 'stok.id', 'stok_etalase.stok_id')
